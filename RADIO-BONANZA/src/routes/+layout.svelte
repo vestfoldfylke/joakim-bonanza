ecb6fc7b9a10256e27173784f1a03330ec6204a3
Joakim <132438695+jxakim@users.noreply.github.com>
2026-09-01 09:36:13 +0200

    Stjal litt fra elevoppfølging

--- RADIO-BONANZA/src/routes/+layout.svelte
+++ RADIO-BONANZA/src/routes/+layout.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import '@digdir/designsystemet-css';
 	import '@digdir/designsystemet-css/theme';
+	import "../style.css"
 	import favicon from '$lib/assets/favicon.svg';
 
 	let { children } = $props();
