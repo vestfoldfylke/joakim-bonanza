2c81c4c5d7db9b5fff043659d12ee21ceb618c1d
Joakim <joakim.solvang.christiansen@gmail.com>
2026-09-14 14:20:45 +0200

    fix: _person don't need a get/set -> nothing else outside of this class gets any value from it

--- Lyttere/Direct/AlarmClockDirect.cs
+++ Lyttere/Direct/AlarmClockDirect.cs
@@ -3,11 +3,9 @@ namespace Lyttere.Direct;
 public class AlarmClockDirect(PersonDirect person)
 {
     // TODO: hold a reference to one Person (constructor parameter, stored in a field).
-    private PersonDirect _person { get; set; } = person;
+    private PersonDirect _person = person;
 
-    public void StartAlarm(string message)
-    {
+    public void StartAlarm(string message) => _person.WakeUp(message);
+
         // TODO: call the person's wake-up method directly, passing `message`.
-        _person.WakeUp(message);
-    }
 }
