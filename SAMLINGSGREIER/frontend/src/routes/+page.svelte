bb66f5a9be98761f402df9cdac1ca4894c9e0606
Joakim <joakim.solvang.christiansen@gmail.com>
2026-09-24 14:01:53 +0200

    fix: add environment and loading functionality

--- SAMLINGSGREIER/frontend/src/routes/+page.svelte
+++ SAMLINGSGREIER/frontend/src/routes/+page.svelte
@@ -1,29 +1,36 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
 	import type { Item } from '$lib/types';
-    
-	const API_URL = 'http://localhost:5199';
+
+    import { PUBLIC_API_URL } from '$env/static/public';
 
 	let items = $state<Item[]>([]);
 	let error = $state<string | null>(null);
+    let loading = $state(true);
 
 	onMount(async () => {
 		try {
-			const response = await fetch(`${API_URL}/items`);
+			const response = await fetch(`${PUBLIC_API_URL}/items`);
 			if (!response.ok) throw new Error(`HTTP ${response.status}`);
 			items = await response.json();
 		} catch (e) {
 			error = e instanceof Error ? e.message : 'Ukjent feil';
-		}
+		} finally {
+            loading = false;
+        }
 	});
 </script>
 
 <h1>Samlingen min</h1>
 
-{#if error}
+{#if loading}
+    <p>Loading data..</p>
+{:else if error}
 	<p>Klarte ikke hente gjenstander: {error}</p>
+{:else if items.length === 0}
+    <p>Det finnes ingen elementer enda.</p>
 {:else}
-	<ul>
+    <ul>
 		{#each items as item (item.id)}
 			<li>{item.name} ({item.category})</li>
 		{/each}
