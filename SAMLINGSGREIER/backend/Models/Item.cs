afc93ea81478e2ee4de4f2e58182133aa10db291
Joakim <joakim.solvang.christiansen@gmail.com>
2026-09-24 10:12:00 +0200

    fix: changed to DateTimeOffset for local timezone

--- SAMLINGSGREIER/backend/Models/Item.cs
+++ SAMLINGSGREIER/backend/Models/Item.cs
@@ -1,6 +1,3 @@
 namespace Backend.Models;
 
-public record Item(Guid Id, string Name, string Category, DateTime DatetimeAdded)
-{
-    
-}
+public record Item(Guid Id, string Name, string Category, DateTimeOffset DatetimeAdded);
