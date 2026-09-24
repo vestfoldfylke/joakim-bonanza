1108cf7b360fc16c5900a420c8d8e436e1f2bf11
Joakim <joakim.solvang.christiansen@gmail.com>
2026-09-24 10:43:10 +0200

    fix: Changed from DateTimeOffset.Now to UtcNow so all timestamps has the same offset

--- SAMLINGSGREIER/backend/Services/ItemRepository.cs
+++ SAMLINGSGREIER/backend/Services/ItemRepository.cs
@@ -6,8 +6,8 @@ public class ItemRepository : IItemRepository
 {
     private static readonly List<Item> _items = new List<Item>
     {
-        new(Guid.NewGuid(), "Gameboy", "Konsoller", DateTimeOffset.Now),
-        new(Guid.NewGuid(), "Morgan Elgitar", "Musikk", DateTimeOffset.Now),
+        new(Guid.NewGuid(), "Gameboy", "Konsoller", DateTimeOffset.UtcNow),
+        new(Guid.NewGuid(), "Morgan Elgitar", "Musikk", DateTimeOffset.UtcNow),
     };
 
     public IEnumerable<Item> GetAllItems() => _items;
