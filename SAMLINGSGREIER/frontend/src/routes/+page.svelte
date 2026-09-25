98130067a171811f25b6e8ed7e837fe53b1cc5ee
Joakim <joakim.solvang.christiansen@gmail.com>
2026-09-25 14:47:20 +0200

    fix: OOPS, wrong place for this

--- SAMLINGSGREIER/frontend/src/routes/+page.svelte
+++ SAMLINGSGREIER/frontend/src/routes/+page.svelte
@@ -9,7 +9,7 @@
 
 	onMount(async () => {
 		try {
-			const response = await fetch(`${PUBLIC_API_URL}/items`.replace(/\/$/, ""))
+			const response = await fetch(`${PUBLIC_API_URL.replace(/\/$/, "")}/items`)
 			if (!response.ok) {
 				throw new Error(`HTTP ${response.status}`)
 			}
